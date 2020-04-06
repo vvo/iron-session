14a162d2c5ee9f38aa0c8319e9a1a7336e375daf
Vincent Voyer <vincent@codeagain.com>
2020-04-06 14:43:06 +0200

    chore(lint): add import plugin

--- example/pages/login.js
+++ example/pages/login.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import useUser from "../lib/hooks/useUser";
 import Layout from "../components/layout";
 import Form from "../components/form";
