231ea24bbbffe3a6eb29d696ab7d78d20f0b7c14
Christian QH <Oxicode@users.noreply.github.com>
2020-05-13 18:18:46 -0500

    Update index.js

--- examples/express/routes/index.js
+++ examples/express/routes/index.js
@@ -7,7 +7,7 @@ var session = ironSession({
   password: process.env.SECRET_COOKIE_PASSWORD,
   cookieOptions: {
     // the next line allows to use the session in non-https environements
-    secure: process.env.NODE_ENV === "production" ? true : false,
+    secure: process.env.NODE_ENV === "production",
   },
 });
 
