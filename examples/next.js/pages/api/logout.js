03b02056194ab83a46863cf0a9eeff80a6024a16
Vincent Voyer <vincent@codeagain.com>
2020-12-09 15:45:45 +0100

    test4

--- examples/next.js/pages/api/logout.js
+++ examples/next.js/pages/api/logout.js
@@ -2,6 +2,6 @@ import withSession from "../../lib/session";
 
 export default withSession(async (req, res) => {
   req.session.destroy();
-  res.setHeader("cache-control", "no-cache, max-age=0");
+  res.setHeader("cache-control", "no-store, max-age=0");
   res.json({ isLoggedIn: false });
 });
