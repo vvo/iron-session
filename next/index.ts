860e4f48b2a5d6766bd7f6f77ce3fba92e5908fd
searchableguy <me@searchableguy.com>
2022-03-16 15:06:23 +0530

    feat(next): Add ability to dynamic options to iron in next middleware functions

--- next/index.ts
+++ next/index.ts
@@ -6,12 +6,23 @@ import type {
 import type { IronSessionOptions } from "iron-session";
 import { getIronSession } from "iron-session";
 import getPropertyDescriptorForReqSession from "../src/getPropertyDescriptorForReqSession";
+import type { IncomingMessage, ServerResponse } from "http";
+
+// Argument types based on getIronSession function
+type GetIronSessionOptions = (
+  request: IncomingMessage,
+  response: ServerResponse,
+) => Promise<IronSessionOptions>;
 
 export function withIronSessionApiRoute(
   handler: NextApiHandler,
-  options: IronSessionOptions,
+  options: IronSessionOptions | GetIronSessionOptions,
 ): NextApiHandler {
   return async function nextApiHandlerWrappedWithIronSession(req, res) {
+    // If options is a function, call it and assign the results back.
+    if (options instanceof Function) {
+      options = await options(req, res);
+    }
     const session = await getIronSession(req, res, options);
 
     // we define req.session as being enumerable (so console.log(req) shows it)
@@ -33,11 +44,15 @@ export function withIronSessionSsr<
   handler: (
     context: GetServerSidePropsContext,
   ) => GetServerSidePropsResult<P> | Promise<GetServerSidePropsResult<P>>,
-  options: IronSessionOptions,
+  options: IronSessionOptions | GetIronSessionOptions,
 ) {
   return async function nextGetServerSidePropsHandlerWrappedWithIronSession(
     context: GetServerSidePropsContext,
   ) {
+    // If options is a function, call it and assign the results back.
+    if (options instanceof Function) {
+      options = await options(context.req, context.res);
+    }
     const session = await getIronSession(context.req, context.res, options);
     Object.defineProperty(
       context.req,
