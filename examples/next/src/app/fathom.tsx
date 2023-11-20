1071ac9955af5d9f54f73e7e63aa274872847f27
Vincent Voyer <vincent@codeagain.com>
2023-11-20 09:48:13 +0100

    add fathom

---
+++ examples/next/src/app/fathom.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { load, trackPageview } from "fathom-client";
+import { useEffect, Suspense } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
+
+function TrackPageView() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  // Load the Fathom script on mount
+  useEffect(() => {
+    load("YKGUEAZB", {
+      auto: false,
+    });
+  }, []);
+
+  // Record a pageview when route changes
+  useEffect(() => {
+    if (!pathname) return;
+
+    trackPageview({
+      url: pathname + searchParams.toString(),
+      referrer: document.referrer,
+    });
+  }, [pathname, searchParams]);
+
+  return null;
+}
+
+export default function Fathom() {
+  return (
+    <Suspense fallback={null}>
+      <TrackPageView />
+    </Suspense>
+  );
+}
--- examples/next/src/app/layout.tsx
+++ examples/next/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Fathom from "./fathom";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-50`}>{children}</body>
+      <body className={`${inter.className} bg-gray-50`}>
+        <Fathom />
+        {children}
+      </body>
     </html>
   );
 }
